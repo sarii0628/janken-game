73c0ff1ee1b4ad7dccac44d46cd1e3a3b05a973a
sarii0628 <9mm.p.bull@gmail.com>
2020-03-14 20:24:58 +0900

    セッターなし

---
+++ battle.php
@@ -0,0 +1,24 @@
+<?php
+require_once('janken.php');
+
+$janken = new Janken($_POST['playerHand']);
+$janken->battle();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf8">
+    <title>じゃんけん</title>
+</head>
+<body>
+    <h1>結果は・・・</h1>
+        <p><?= $janken->getResult() ?>！</p>
+
+        あなた：<?= $janken->getPlayerHand() ?><br>
+        コンピューター：<?= $janken->getPcHand() ?><br>
+
+        <p><a href="index.php">>>もう一回勝負する</a></p>
+    </div>
+</body>
+</html>
