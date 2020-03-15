6bfcc3d80f82026f215ad61e61de17502ddcc35c
sarii0628 <9mm.p.bull@gmail.com>
2020-03-15 11:07:23 +0900

    選択肢に絵文字つき

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
 	<div class="form-box">
 		<form action="battle.php" method="post">
 			<label>
-				<input type="radio" title="playerHand" name="playerHand" value="グー" checked>グー
+				<input type="radio" title="playerHand" name="playerHand" value="グー" checked>グー&#x270a;
 			</label>
 			<label>
-				<input type="radio" title="playerHand" name="playerHand" value="チョキ">チョキ
+				<input type="radio" title="playerHand" name="playerHand" value="チョキ">チョキ&#x270c;
 			</label>
 			<label>
-				<input type="radio" title="playerHand" name="playerHand" value="パー">パー
+				<input type="radio" title="playerHand" name="playerHand" value="パー">パー&#x1f590;
 			</label>
 			<button type="submit" class="battle-button">勝負する！</button>
 		</form>
